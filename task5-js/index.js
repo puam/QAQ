cd553bcbfc4b649446e2444117da3bb1e021b3f6
puam <2870548696@qq.com>
2019-05-04 10:26:15 +0800

    w1w1w1

--- task5-js/index.js
+++ task5-js/index.js
@@ -14,15 +14,18 @@ function logIn() {
             // readyState,请求的状态。xhr.status,http请求状态码。
             if (xhr.readyState==4){
                 if ((xhr.status >=200 && xhr.status<300) || xhr.status == 304) {
-                    alert(xhr.readyState);
+                    console.log(xhr.responseText);
                 }else {
                     alert("请求失败："+xhr.readyState);
                 }
             }
-        }
+        };
+        xhr.open("post"," /carrots-admin-ajax/a/login",true);
+        //url,后台地址加，登陆请求地址。
+        xhr.setRequestHeader("content-type","application/x-www-form-urlencoded,charset=uff-8");//使用send方法传送参数的时候，需要设定
+        //content-type头的信息，模拟http post方法发送一个表单。这样服务器才知道我们要发送的什么？
+        xhr.setRequestHeader("Accept", "application/json,text/plain,*/*");//服务器向我们发送的。
+        xhr.send('name='+name+'&pwd='+pwd);//json格式的数据
     }
-    //open方法启动ajax，url要请求的接口地址。true，异步请求。
-    xhr.open("post"," /carrots-admin-ajax/a/login",true);
-    var data="name"+encodeURIComponent(name)+"pwd"+encodeURIComponent(pwd);
-    xhr.send(data);
+
 }
